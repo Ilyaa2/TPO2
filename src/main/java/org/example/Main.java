7737d42fbc0753ca5302a20c0d82a60df05238ba
ivanmorikov <ivanmorikov48@gmail.com>
2024-04-15 10:38:20 +0300

    Added csv files for trigonometric functions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,11 +7,14 @@ import org.example.trigonometric.*;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         writeLogarithmicCsv();
+        writeTrigonometricCsv();
         writeFunctionsSystemCsv();
     }
 
@@ -19,13 +22,14 @@ public class Main {
         try (FileWriter lnFile = new FileWriter("src/main/resources/csv/ln.csv");
              FileWriter log2File = new FileWriter("src/main/resources/csv/log2.csv");
              FileWriter log3File = new FileWriter("src/main/resources/csv/log3.csv");
-             FileWriter log5File = new FileWriter("src/main/resources/csv/log5.csv")
+             FileWriter log5File = new FileWriter("src/main/resources/csv/log5.csv");
         ) {
             var precision = 0.00001;
             var ln = new Ln();
             var log2 = new Log(ln, 2);
             var log3 = new Log(ln, 3);
             var log5 = new Log(ln, 5);
+
 
             var listLog2 = new ArrayList<CsvWriter.Pair>();
             var listLog3 = new ArrayList<CsvWriter.Pair>();
@@ -42,12 +46,55 @@ public class Main {
             CsvWriter.writeResultToCSV(listLog5, log5File);
             CsvWriter.writeResultToCSV(listLn, lnFile);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Файл невозможно открыть");
         }
     }
 
-    public static void writeFunctionsSystemCsv(){
-        try (FileWriter FunctionsSystemFile = new FileWriter("src/main/resources/csv/systemFunctions.csv")){
+    public static void writeTrigonometricCsv() {
+        try (FileWriter sinFile = new FileWriter("src/main/resources/csv/sin.csv");
+                FileWriter cosFile = new FileWriter("src/main/resources/csv/cos.csv");
+                FileWriter tanFile = new FileWriter("src/main/resources/csv/tan.csv");
+                FileWriter cotFile = new FileWriter("src/main/resources/csv/cot.csv");
+                FileWriter cscFile = new FileWriter("src/main/resources/csv/csc.csv");
+                FileWriter secFile = new FileWriter("src/main/resources/csv/sec.csv");
+        ) {
+            double precision = 0.0001;
+            Sin sin = new Sin();
+            Cos cos = new Cos(sin);
+            Tan tan = new Tan(sin);
+            Cot cot = new Cot(sin);
+            Csc csc = new Csc(sin);
+            Sec sec = new Sec(sin);
+
+            ArrayList<CsvWriter.Pair> listSin = new ArrayList<>();
+            ArrayList<CsvWriter.Pair> listCos = new ArrayList<>();
+            ArrayList<CsvWriter.Pair> listTan = new ArrayList<>();
+            ArrayList<CsvWriter.Pair> listCot = new ArrayList<>();
+            ArrayList<CsvWriter.Pair> listSec = new ArrayList<>();
+            ArrayList<CsvWriter.Pair> listCsc = new ArrayList<>();
+            for (double i = 0.1; i <= 2d; i += 0.1) {
+                listSin.add(new CsvWriter.Pair(i, sin.calculate(i, precision)));
+                listCos.add(new CsvWriter.Pair(i, cos.calculate(i, precision)));
+                listTan.add(new CsvWriter.Pair(i, tan.calculate(i, precision)));
+                listCot.add(new CsvWriter.Pair(i, cot.calculate(i, precision)));
+                listSec.add(new CsvWriter.Pair(i, sec.calculate(i, precision)));
+                listCsc.add(new CsvWriter.Pair(i, csc.calculate(i, precision)));
+            }
+            CsvWriter.writeResultToCSV(listSin, sinFile);
+            CsvWriter.writeResultToCSV(listCos, cosFile);
+            CsvWriter.writeResultToCSV(listTan, tanFile);
+            CsvWriter.writeResultToCSV(listCot, cotFile);
+            CsvWriter.writeResultToCSV(listSec, secFile);
+            CsvWriter.writeResultToCSV(listCsc, cscFile);
+
+
+        } catch (IOException e) {
+            System.out.println("Файл невозможно открыть");
+        }
+    }
+
+    public static void writeFunctionsSystemCsv() {
+        try (FileWriter FunctionsSystemFile = new FileWriter("src/main/resources/csv/systemFunctions.csv")) {
             var precision = 0.00001;
             Sin sin = new Sin();
             Ln ln = new Ln();
