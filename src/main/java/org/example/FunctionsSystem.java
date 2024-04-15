226e415b6393fc58ac51d83b96f832def5039bd6
ivanmorikov <ivanmorikov48@gmail.com>
2024-04-15 18:19:51 +0300

    Function was changed

--- src/main/java/org/example/FunctionsSystem.java
+++ src/main/java/org/example/FunctionsSystem.java
@@ -4,6 +4,8 @@ import org.example.logariphmic.Ln;
 import org.example.logariphmic.Log;
 import org.example.trigonometric.*;
 
+import javax.naming.PartialResultException;
+
 public class FunctionsSystem {
 
     private Sin sin;
@@ -33,22 +35,24 @@ public class FunctionsSystem {
     public double calculate(double x, double precision) {
         double result = 0;
         if (x <= 0) {
-            result = ((Math.pow(Math.pow((Math.pow((((((Math.pow(csc.calculate(x, precision), 3) * sin.calculate(x, precision) * sin.calculate(x, precision) + Math.pow(sec.calculate(x, precision), 2))
-                    / tan.calculate(x, precision) / cot.calculate(x, precision))
-                    + Math.pow(sec.calculate(x, precision), 3)) + csc.calculate(x, precision))
-                    * sin.calculate(x, precision)), 3) + (Math.pow(tan.calculate(x, precision), 2) - sec.calculate(x, precision)))
-                    / cos.calculate(x, precision), 3), 2) / cot.calculate(x, precision))
-                    / Math.pow(Math.pow(cos.calculate(x, precision) / ((cos.calculate(x, precision) - csc.calculate(x, precision))
-                    / (sec.calculate(x, precision))), 2) * tan.calculate(x, precision), 3)
+//            result = ((Math.pow(Math.pow((Math.pow((((((Math.pow(csc.calculate(x, precision), 3) * sin.calculate(x, precision) * sin.calculate(x, precision) + Math.pow(sec.calculate(x, precision), 2))
+//                    / tan.calculate(x, precision) / cot.calculate(x, precision))
+//                    + Math.pow(sec.calculate(x, precision), 3)) + csc.calculate(x, precision))
+//                    * sin.calculate(x, precision)), 3) + (Math.pow(tan.calculate(x, precision), 2) - sec.calculate(x, precision)))
+//                    / cos.calculate(x, precision), 3), 2) / cot.calculate(x, precision))
+//                    / Math.pow(Math.pow(cos.calculate(x, precision) / ((cos.calculate(x, precision) - csc.calculate(x, precision))
+//                    / (sec.calculate(x, precision))), 2) * tan.calculate(x, precision), 3)
+//
+//
+//                    + Math.pow((csc.calculate(x, precision) + (tan.calculate(x, precision) + Math.pow(cot.calculate(x, precision) * (cos.calculate(x, precision) + (Math.pow(tan.calculate(x, precision), 2) *
+//                    ((tan.calculate(x, precision)) / (sin.calculate(x, precision))))), 3)))
+//
+//
+//                    + (sec.calculate(x, precision) - csc.calculate(x, precision))
+//                    / Math.pow(sin.calculate(x, precision) - ((sec.calculate(x, precision) - sin.calculate(x, precision) - sec.calculate(x, precision)) + (cos.calculate(x, precision) *
+//                    ((cot.calculate(x, precision) - Math.pow(sec.calculate(x, precision), 2) * ((sin.calculate(x, precision) + sin.calculate(x, precision))) / (Math.pow(csc.calculate(x, precision), 3)))) / (sin.calculate(x, precision)))), 3), 2));
 
-
-                    + Math.pow((csc.calculate(x, precision) + (tan.calculate(x, precision) + Math.pow(cot.calculate(x, precision) * (cos.calculate(x, precision) + (Math.pow(tan.calculate(x, precision), 2) *
-                    ((tan.calculate(x, precision)) / (sin.calculate(x, precision))))), 3)))
-
-
-                    + (sec.calculate(x, precision) - csc.calculate(x, precision))
-                    / Math.pow(sin.calculate(x, precision) - ((sec.calculate(x, precision) - sin.calculate(x, precision) - sec.calculate(x, precision)) + (cos.calculate(x, precision) *
-                    ((cot.calculate(x, precision) - Math.pow(sec.calculate(x, precision), 2) * ((sin.calculate(x, precision) + sin.calculate(x, precision))) / (Math.pow(csc.calculate(x, precision), 3)))) / (sin.calculate(x, precision)))), 3), 2));
+            result = (((Math.pow(Math.pow(((Math.pow((((((((((Math.pow(csc.calculate(x,precision), 3)) * sin.calculate(x, precision)) * sin.calculate(x, precision) ) + Math.pow(sec.calculate(x,precision), 2)) / tan.calculate(x, precision)) / cot.calculate(x,precision) ) + Math.pow(sec.calculate(x,precision), 3)) + csc.calculate(x,precision)) * sin.calculate(x,precision)), 3) + (Math.pow(tan.calculate(x,precision), 2) - sec.calculate(x,precision) )) / Math.pow(cos.calculate(x,precision), 2)), 3), 2) / Math.pow(cot.calculate(x, precision), 3)) / Math.pow(((cos.calculate(x, precision) / Math.pow(((cos.calculate(x, precision) - csc.calculate(x, precision)  ) / sec.calculate(x, precision)), 2)) * tan.calculate(x, precision) ), 3)) + Math.pow(((csc.calculate(x, precision) + (tan.calculate(x, precision) + Math.pow((cot.calculate(x, precision) * (cos.calculate(x, precision) + (Math.pow(tan.calculate(x, precision), 2) * (tan.calculate(x, precision) / sin.calculate(x, precision))))), 3))) + ((sec.calculate(x, precision) - csc.calculate(x, precision)  ) / (sin.calculate(x, precision) - (((sec.calculate(x,precision) - sin.calculate(x, precision)  ) - sec.calculate(x, precision)  ) + (cos.calculate(x, precision) * Math.pow((((cot.calculate(x, precision) - Math.pow(sec.calculate(x, precision), 2)) * Math.pow(((sin.calculate(x, precision) + sin.calculate(x, precision) ) / Math.pow(csc.calculate(x, precision), 3)), 3)) / sin.calculate(x,precision) ), 3)))))), 2));
         } else {
             result = (Math.pow((((log_3.calculate(x, precision) + log_3.calculate(x, precision)) - log_2.calculate(x, precision)) / log_3.calculate(x, precision)), 2) *
                     (Math.pow(log_5.calculate(x, precision), 3) - log_2.calculate(x, precision)));
