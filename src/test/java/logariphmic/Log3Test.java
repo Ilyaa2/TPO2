e9073043bc8287cfc699bc7f407397254a782b40
User <ilya31zagor03@mail.ru>
2024-04-15 15:52:56 +0300

    add some test for log

--- src/test/java/logariphmic/Log3Test.java
+++ src/test/java/logariphmic/Log3Test.java
@@ -18,10 +18,40 @@ public class Log3Test {
     private Ln mockLn;
 
     @Test
-    void log3Test() {
+    void log3Test1() {
         double x = 81;
         double precision = 0.0001;
         double expectedAnswer = 4;
+
+        when(mockLn.calculate(x, precision)).thenReturn(Math.log(x));
+        when(mockLn.calculate(base, precision)).thenReturn(Math.log(base));
+
+        Log log = new Log(mockLn, base);
+        var result = log.calculate(x, precision);
+        double diff = Math.abs(result - expectedAnswer);
+        Assertions.assertTrue(diff <= precision);
+    }
+
+    @Test
+    void log3Test2() {
+        double x = 3;
+        double precision = 0.0001;
+        double expectedAnswer = 1;
+
+        when(mockLn.calculate(x, precision)).thenReturn(Math.log(x));
+        when(mockLn.calculate(base, precision)).thenReturn(Math.log(base));
+
+        Log log = new Log(mockLn, base);
+        var result = log.calculate(x, precision);
+        double diff = Math.abs(result - expectedAnswer);
+        Assertions.assertTrue(diff <= precision);
+    }
+
+    @Test
+    void log3Test3() {
+        double x = 243;
+        double precision = 0.0001;
+        double expectedAnswer = 5;
 
         when(mockLn.calculate(x, precision)).thenReturn(Math.log(x));
         when(mockLn.calculate(base, precision)).thenReturn(Math.log(base));
